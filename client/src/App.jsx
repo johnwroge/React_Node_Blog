d02db6c6ecd1da1299bc6e53eb9bb5635f62153e
John Wroge <72668920+johnwroge@users.noreply.github.com>
2023-12-04 21:59:31 -0500

    added login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
-
+import Login from './components/login'
 import './App.css'
 
 function App() {
   return (
     <>
       <h1> This will be a title to the blog</h1>
+      <Login/>
     </>
   )
 }
