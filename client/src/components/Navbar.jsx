a014b2c222b0f684bbc81f57255441802662f052
John Wroge <72668920+johnwroge@users.noreply.github.com>
2023-12-13 16:13:05 -0500

    finished nav bar!

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom';
+import Hamburger from 'hamburger-react'
 import './navbar.css'
 
 const Navbar = () => {
+  const [showNavbar, setShowNavbar] = useState(false)
+
+
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar)
+  }
+
   return (
     <nav className="navbar">
       <div className="container">
-        <div className="logo">
+      <div className="logo">
           Logo
         </div>
-        <div className="nav-elements">
+        <div className = "menu-icon" onClick={handleShowNavbar}>
+        <Hamburger toggled={(showNavbar)}  />
+        
+        </div>
+        <div className={`nav-elements  ${showNavbar && 'active'}`}>
           <ul>
             <li>
               <NavLink to="/">Home</NavLink>
