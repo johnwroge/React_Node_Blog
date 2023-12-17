71ef243054f2072008b26b016e1bfa1bbc4ce293
John Wroge <72668920+johnwroge@users.noreply.github.com>
2023-12-16 20:09:24 -0500

    finished most of post

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './post.css';
 
 // eslint-disable-next-line react/prop-types
 const Post = ({title, date, description, img}) => {
+    const [isClicked, setIsClicked] = useState(false);
+
+    const handleClick = () => {
+      setIsClicked(!isClicked);
+    };
+  
    
   return (
-    <div className='Post'>
+    <div className={`Post ${isClicked ? 'clicked' : ''}`} onClick={handleClick}>
         <img src={img} />
         
         <h3> {title} </h3>
